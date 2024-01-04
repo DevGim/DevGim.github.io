741ec7349950dc4c2499970edf200c067c401bb9
DevGim <dm8.kim@gmail.com>
2024-01-04 17:52:25 +0900

    change the tile of the app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'My Flutter App',
+      title: 'Map of Child Allowance in Seongnam City',
       home: MyHomePage(),
     );
   }
