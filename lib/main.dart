75941ea7cce87a95c6b97412db6965a35f677650
DevGim <dm8.kim@gmail.com>
2024-01-04 17:48:24 +0900

    extract a file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart';
-import './marker_info.dart';
-import './with_geo_hard.dart';
-
-const MaterialColor iconColor = Colors.red;
-const double iconSize = 40;
+import 'screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,68 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'My Flutter App',
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('성남시 아동수당 신한카드 사용처'),
-      ),
-      body: const FlutterApp(),
-    );
-  }
-}
-
-class FlutterApp extends StatelessWidget {
-  const FlutterApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: LatLng(37.411549, 127.1298651),
-        initialZoom: 14.5,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
-        ),
-        MarkerLayer(
-          markers: markers
-              .map((markerInfo) => Marker(
-                    width: 200,
-                    height: iconSize,
-                    point: LatLng(markerInfo.latitude, markerInfo.longitude),
-                    child: Column(
-                      children: [
-                        Icon(
-                          Icons.location_pin,
-                          color: iconColor,
-                          size: iconSize,
-                        ),
-                        Text(markerInfo.storeName),
-                      ],
-                    ),
-                  ))
-              .toList(),
-        ),
-        RichAttributionWidget(
-          attributions: [
-            TextSourceAttribution(
-              'OpenStreetMap contributors',
-              onTap: () =>
-                  launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
